49173b6de5e1b8972cb8f565fe0da0aa0d82cd3a
allyson <allyson.b.rodrigues@gmail.com>
2020-02-20 21:46:44 -0300

    aula 137

--- lib/pages/car_detail.dart
+++ lib/pages/car_detail.dart
@@ -12,15 +12,66 @@ class CarDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text(car.nome)),
+      appBar: AppBar(
+        title: Text(car.nome),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMap,
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: _onClickVideo,
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) => _onClickPopupMenu(value),
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  value: "Editar",
+                  child: Text("Editar"),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text("Deletar"),
+                ),
+                PopupMenuItem(
+                  value: "Share",
+                  child: Text("Share"),
+                ),
+
+              ];
+            },
+          )
+        ],
+      ),
       body: _body(),
     );
   }
 
   _body() {
     return Container(
-      padding: EdgeInsets.all(16),
-        child: Image.network(car.urlFoto)
-    );
+        padding: EdgeInsets.all(16), child: Image.network(car.urlFoto));
+  }
+
+  void _onClickMap() {}
+
+
+
+  void _onClickVideo() {
+  }
+
+  _onClickPopupMenu(String value) {
+    switch(value) {
+      case "Editar":
+        print("Editar!");
+        break;
+      case "Deletar":
+        print("Deletar!");
+        break;
+      case "Share":
+        print("Share");
+        break;
+    }
   }
 }
