ba47c4542c79015e51f028ed09101cb2791148bc
AllysonBRodrigues <allyson.b.rodrigues@gmail.com>
2020-05-21 09:29:52 -0300

    Aula 190

--- lib/pages/car_listview.dart
+++ lib/pages/car_listview.dart
@@ -19,49 +19,60 @@ class CarListView extends StatelessWidget {
         itemCount: cars != null ? cars.length : 0,
         itemBuilder: (BuildContext context, int index) {
           Car car = cars[index];
-          return Card(
-            child: Container(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: CachedNetworkImage(
-                      imageUrl: car.urlFoto ?? "https://saints-auto.com/wp-content/uploads/2017/06/car-placeholder-2-700.jpg",
-                      width: 250,
-                    ),
-                  ),
-                  Text(
-                    car.nome,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Text(
-                    "Descrição...",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  ButtonBar(
+          return Container(
+            height: 280,
+            child: InkWell(
+              onTap: (){
+                push(context, CarDetail(car));
+              },
+              onLongPress: (){
+                _onLongClickCar(context, car);
+              },
+              child: Card(
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      FlatButton(
-                        child: const Text(
-                          'DETALHES',
+                      Center(
+                        child: CachedNetworkImage(
+                          imageUrl: car.urlFoto ?? "https://saints-auto.com/wp-content/uploads/2017/06/car-placeholder-2-700.jpg",
+                          width: 250,
                         ),
-                        onPressed: () {
-                          push(context, CarDetail(car));
-                        },
                       ),
-                      FlatButton(
-                        child: const Text('SHARE'),
-                        onPressed: () {
-                          /* ... */
-                        },
+                      Text(
+                        car.nome,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Text(
+                        "Descrição...",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      ButtonBar(
+                        children: <Widget>[
+                          FlatButton(
+                            child: const Text(
+                              'DETALHES',
+                            ),
+                            onPressed: () {
+                              push(context, CarDetail(car));
+                            },
+                          ),
+                          FlatButton(
+                            child: const Text('SHARE'),
+                            onPressed: () {
+                              /* ... */
+                            },
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           );
@@ -73,4 +84,30 @@ class CarListView extends StatelessWidget {
   @override
   // TODO: implement wantKeepAlive
   bool get wantKeepAlive => true;
+}
+
+void _onLongClickCar(BuildContext context, Car car) {
+  showDialog(context: context, builder: (context){
+    return SimpleDialog(
+      title: Text(car.nome),
+      children: <Widget>[
+        ListTile(
+          title: Text("Detalhe"),
+          leading: Icon(Icons.directions_car),
+          onTap: (){
+            Navigator.pop(context);
+            push(context, CarDetail(car));
+          },
+        ),
+        ListTile(
+          title: Text("Compartilhar"),
+          leading: Icon(Icons.share),
+          onTap: (){
+           print("Share");
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  });
 }
